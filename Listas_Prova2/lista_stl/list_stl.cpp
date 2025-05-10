d4a5943ca3a0fcdebb671a8f0e8d510256dd3b7b
luizabbasseto <luizabasseto.1@gmail.com>
2025-05-10 17:16:18 -0300

    tentativa de terminar stl

--- Listas_Prova2/lista_stl/list_stl.cpp
+++ Listas_Prova2/lista_stl/list_stl.cpp
@@ -50,11 +50,11 @@ vector<int> ListSTL::list_concat(list &list1, list &list2)
         vec[i] = list1[i];
         i++;
     }
-    int n=i;
-    i=0;
+    int n = i;
+    i = 0;
     while (!list2.empty())
     {
-        vec[n+i] = list1[i];
+        vec[n + i] = list1[i];
         i++;
     }
     return vec;
@@ -65,20 +65,42 @@ bool ListSTL::check_brackets(string expression)
     stack<char> stk;
     for (size_t i = 0; i < expression.length; i++)
     {
-        if(expression[i]=='(' || expression[i]==')' || expression[i]=='[' || expression[i]==']'){
+        if (expression[i] == '(' || expression[i] == '[' || expression[i] == '{')
+        {
             stk.push(expression[i]);
         }
+
+        if ((expression[i] == ')' && stk.top() == '(') || (expression[i] == ']' && stk.top() == '[') || (expression[i] == '}' && stk.top() == '{'))
+        {
+            stk.pop();
+        }
+        else
+        {
+            return false;
+        }
     }
-    
+
+    if (stk.empty())
+    {
+        return true;
+    }
 }
 
 vector<string> ListSTL::vectorize_expression(string expression)
 {
     vector<string> exp;
+    vector<string> temp;
+    int j = 0;
     for (size_t i = 0; i < expression.length(); i++)
     {
-        if(expression[i]!=' '){
-            exp.push_back(expression[i]);
+        if (expression[i] != ' ')
+        {
+            temp[j] += expression[i];
+        }
+        else
+        {
+            exp.push_back(temp);
+            j++;
         }
     }
     return exp;
@@ -86,16 +108,133 @@ vector<string> ListSTL::vectorize_expression(string expression)
 
 float ListSTL::calc_posfix(string expression)
 {
+    vector<string> exp = vectorize_expression(expression);
+    stack<float> stk;
+    for (size_t i = 0; i < exp.size(); i++)
+    {
+        if (stoi(exp[i]).isdigit())
+        {
+            stk.push(exp[i]);
+        }
+
+        if (exp[i] == '+' || exp[i] == '-' || exp[i] == '*' || exp[i] == '/')
+        {
+            int b = stk.top();
+            stk.pop;
+            int a = stk.top();
+            stk.pop;
+            int op = stoi(exp[i]) float res = a, op, b;
+            stk.push(res);
+        }
+    }
+
+    return stk.top();
 }
 
 bool ListSTL::check_posfix(string expression)
 {
+    vector<string> exp = vectorize_expression(expression);
+    stack<float> stk;
+    int n = 0;
+    for (size_t i = 0; i < exp.size(); i++)
+    {
+        if (stoi(exp[i]).isdigit())
+        {
+            stk.push(exp[i]);
+            n++;
+        }
+
+        if (exp[i] == '+' || exp[i] == '-' || exp[i] == '*' || exp[i] == '/')
+        {
+            if (n >= 2)
+            {
+                stk.pop();
+                n = 0;
+            }
+        }
+    }
+
+    if (n == 1)
+        return true;
+
+    return false;
 }
 
 float ListSTL::calc_infix(string expression)
 {
+    vector<string> exp = vectorize_expression(expression);
+    stack<float> operandos;
+    stack<char> operadores;
+
+    for (size_t i = 0; i < exp.size(); i++)
+    {
+        if (stoi(exp[i]).isdigit())
+        {
+            operandos.push(exp[i]);
+        }
+        if (exp[i] == '(')
+            operadores.push(exp[i]);
+
+        if (exp[i] == '+' || exp[i] == '-')
+        {
+            operadores.push(exp[i]);
+        }
+
+        if (exp[i] == '*' || exp[i] == '/')
+        {
+            int b = operandos.top();
+            operandos.pop();
+            int a = operandos.top;
+            operandos.pop();
+            char op = operadores.top();
+            operadores.pop();
+            int res = a, op, b;
+
+            operandos.push(res);
+        }
+
+        if (exp[i] == ')')
+        {
+            do
+            {
+                int b = operandos.top();
+                operandos.pop();
+                int a = operandos.top;
+                operandos.pop();
+                char op = operadores.top();
+                operadores.pop();
+                int res = a, op, b;
+
+                operandos.push(res);
+            } while (operadores.top() == '(');
+
+            operadores.pop();
+        }
+    }
+
+    return operandos.top();
 }
 
 string ListSTL::posfix_to_infix(string expression)
 {
+    stack<string> infix;
+    for (size_t i = 0; i < expression.length(); i++)
+    {
+        if (stoi(expression[i]).isdigit())
+        {
+            infix.push(expression[i]);
+        }
+
+        if (expression[i] == '+' || expression[i] == '-' || expression[i] == '*' || expression[i] == '/')
+        {
+            string a = '(' + infix.top();
+            infix.pop();
+            string b = infix.top();
+            infix.pop();
+            string exp = strcat(exp, a, expression[i], b, ')');
+        }
+    }
+
+    return infix;
 }
+
