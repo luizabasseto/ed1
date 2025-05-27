d7b3bd805d04ff01d89d668932e62de463099d6b
Luiza Batista Basseto <luizabasseto.1@gmail.com>
2025-05-27 15:29:46 -0300

    changerecursividade

---
+++ Recursividade/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+
+using namespace std;
+
+int fib(int n, vector<int> &mem)
+{
+    if (n <= 1)
+    {
+        return n;
+    }
+    else if (n >= mem.size())
+    {
+        cout << "N: " << n << "- Valor: " << mem[n] << endl;
+        mem.push_back(fib(n - 1, mem));
+    }
+
+    return fib(n - 2, mem) + fib(n - 1, mem);
+}
+
+int main()
+{
+
+    int n = 0;
+    cout << "Digite o valor de n" << endl;
+    cin >> n;
+
+    vector<int> mem = {0};
+
+    cout << "Resultado: " << fib(n, mem);
+    cout << endl;
+
+    return 0;
+}
